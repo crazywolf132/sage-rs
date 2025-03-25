32a63c29bab15cf684853f10a1500963ab252629
crazywolf132 <crazywolf132@gmail.com>
2025-03-25 16:33:17 -0700

    fix: removed broken update notifier

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,9 @@
 use sage::cli::Run;
 use clap::Parser;
 use std::process::ExitCode;
-use tiny_update_notifier::check_github;
 
 #[tokio::main]
 async fn main() -> ExitCode {
-    // Checks for version updates 
-    check_github(
-        env!("CARGO_PKG_VERSION"),
-        env!("CARGO_PKG_NAME"),
-        env!("CARGO_PKG_REPOSITORY")
-    );
 
     // Runs the main CLI
     match sage::cli::Cmd::parse().run().await {
